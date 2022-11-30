48af4a4c5ece14a19c29b15ff77b360c4b57d996
Artem Demyanov <hantagu43@gmail.com>
2022-11-30 19:19:42 +0300

    v1.1

--- Main.kt
+++ Main.kt
@@ -7,9 +7,10 @@ fun main()
 {
     val re = listOf(
         Regex("""^(?<operation>история) (?<arg1>арифметические|прогрессионные|геометрические)$"""),
-        Regex("""^(?<arg1>((?:\+|\-)?\d+)) ?(?<operation>\+|\-|\*|\/) ?(?<arg2>((?:\+|\-)?\d+))$"""),
-        Regex("""^(?<operation>арифметическая|геометрическая) (?<arg1>((?:\+|\-)?\d+)) (?<arg2>((?:\+|\-)?\d+)) (?<arg3>((?:\+|\-)?\d+))$"""),
-        Regex("""^(?<operation>периметр|площадь) (?<arg1>(?:((?:\+|\-)?\d+) ?)+)$""")
+        Regex("""^(?<arg1>(?:\+|\-)?\d+) *(?<operation>\+|\-|\*|\/) *(?<arg2>(?:\+|\-)?\d+)$"""),
+        Regex("""^(?<operation>арифметическая|геометрическая) (?<arg1>(?:\+|\-)?\d+) (?<arg2>(?:\+|\-)?\d+) (?<arg3>(?:\+|\-)?\d+)$"""),
+        Regex("""^(?<operation>периметр)(?<arg1>(?: \+?\d+){2,})$"""),
+        Regex("""^(?<operation>площадь) (?<arg1>\+?\d+)(?: (?<arg2>\+?\d+))?$""")
     )
 
     val logger = Logger("logs", "arithmetic.log", "progression.log", "geometric.log")
@@ -18,34 +19,31 @@ fun main()
     val input = readln()
 
     var regexResult: MatchResult? = null
-    for (i in re)
-    {
-        regexResult = i.matchEntire(input)
 
+    for (r in re) {
+        regexResult = r.matchEntire(input)
         if (regexResult !== null)
             break
     }
 
-    if (regexResult === null)
-    {
+    if (regexResult === null) {
         println("Недопустимое выражение")
         return
     }
 
-    val operation = try { regexResult.groups["operation"]?.value } catch (_: Exception) { null }
-    val arg1 = try { regexResult.groups["arg1"]?.value } catch (_: Exception) { null }
+    val operation = regexResult.groups["operation"]?.value
+    val arg1 = regexResult.groups["arg1"]?.value
     val arg2 = try { regexResult.groups["arg2"]?.value } catch (_: Exception) { null }
     val arg3 = try { regexResult.groups["arg3"]?.value } catch (_: Exception) { null }
 
     when
     {
         operation == "история" && arg1 !== null -> {
-            println(when (arg1) {
-                "арифметические" -> logger.get(LogType.ARITHMETIC)
-                "прогрессионные" -> logger.get(LogType.PROGRESSION)
-                "геометрические" -> logger.get(LogType.GEOMETRIC)
-                else -> null
-            }?.joinToString("\n"))
+            when (arg1) {
+                "арифметические" -> println(logger.get(LogType.ARITHMETIC).joinToString("\n"))
+                "прогрессионные" -> println(logger.get(LogType.PROGRESSION).joinToString("\n"))
+                "геометрические" -> println(logger.get(LogType.GEOMETRIC).joinToString("\n"))
+            }
         }
 
         operation == "+" && arg1 !== null && arg2 !== null -> {
@@ -76,32 +74,45 @@ fun main()
             logger.add(LogType.ARITHMETIC, "$a $operation $b = $result")
         }
 
-        operation == "арифметическая" && arg1 !== null && arg2 !== null -> {
+        operation == "арифметическая" && arg1 !== null && arg2 !== null && arg3 !== null -> {
             val start = arg1.toInt()
             val step = arg2.toInt()
-            val n = arg3!!.toInt()
+            val n = arg3.toInt()
             val result = Progression.arithmetic(start, step, n)
             logger.add(LogType.PROGRESSION, "$operation; $step;\n$result")
         }
 
-        operation == "геометрическая" && arg1 !== null && arg2 !== null -> {
+        operation == "геометрическая" && arg1 !== null && arg2 !== null && arg3 !== null -> {
             val start = arg1.toInt()
             val step = arg2.toInt()
-            val n = arg3!!.toInt()
+            val n = arg3.toInt()
             val result = Progression.geometric(start, step, n)
             logger.add(LogType.PROGRESSION, "$operation; $step;\n$result")
         }
 
         operation == "периметр" && arg1 !== null -> {
-            val args = arg1.split(" ").map { it.toInt() }
+            val args = arg1.split(" ").map { it.trim().toInt() }
             val result = Geometric.perimeter(args)
             logger.add(LogType.GEOMETRIC, "$operation $args = $result")
         }
 
-        operation == "площадь" && arg1 !== null -> {
-            val args = arg1.split(" ").map { it.toInt() }
-            val result = if (args.size == 1) Geometric.circleSquare(args[0]) else Geometric.rectangleSquare(args[0], args[1])
-            logger.add(LogType.GEOMETRIC, "$operation $args = $result")
+        operation == "площадь" ->
+        {
+            if (arg1 !== null && arg2 !== null)
+            {
+                val a = arg1.toInt()
+                val b = arg2.toInt()
+                val result = Geometric.rectangleSquare(a, b)
+
+                logger.add(LogType.GEOMETRIC, "$operation [$a, $b] = $result")
+            }
+            else if (arg1 !== null)
+            {
+                val a = arg1.toInt()
+                val result = Geometric.circleSquare(a)
+
+                logger.add(LogType.GEOMETRIC, "$operation [$a] = $result")
+            }
         }
     }
 }
